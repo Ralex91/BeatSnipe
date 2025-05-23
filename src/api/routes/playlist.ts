e93d67eb9af4a96c5b8bb8715f8a0d4ec5baba24
Ralex <95540504+Ralex91@users.noreply.github.com>
2025-05-24 00:19:11 +0200

    refactor(api): remove user-agent authorization checks from playlist function

--- src/api/routes/playlist.ts
+++ src/api/routes/playlist.ts
@@ -4,16 +4,6 @@ import { Request, Response } from "express"
 const leaderboardList = ["scoresaber", "beatleader"]
 
 export default async function playlist(req: Request, res: Response) {
-  const userAgent = req.headers["user-agent"]
-
-  if (!userAgent) {
-    return res.status(401).json({ code: 401, message: "Unauthorized" })
-  }
-
-  if (!userAgent.includes("PlaylistManager")) {
-    return res.status(401).json({ code: 401, message: "Unauthorized" })
-  }
-
   if (!leaderboardList.includes(req.params.leaderboard)) {
     return res.status(404).json({ code: 404, message: "Playlist not found" })
   }
